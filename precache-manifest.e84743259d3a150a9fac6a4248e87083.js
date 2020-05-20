ed5a80b0a74667bb2b5281b57ff4673bd5a19299
5377 <pradipamate07@gmail.com>
2020-05-20 11:37:59 +0530

    Updates

---
+++ precache-manifest.e84743259d3a150a9fac6a4248e87083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125508e8add18347514c0014eca449e5",
+    "url": "/todo_hooks/index.html"
+  },
+  {
+    "revision": "73f79fe2b3a8e4a9aac5",
+    "url": "/todo_hooks/static/css/main.7f611bbe.chunk.css"
+  },
+  {
+    "revision": "928b8ad548aae563efba",
+    "url": "/todo_hooks/static/js/2.b64bdefd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_hooks/static/js/2.b64bdefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f79fe2b3a8e4a9aac5",
+    "url": "/todo_hooks/static/js/main.f9e4310d.chunk.js"
+  },
+  {
+    "revision": "3f4bfc75c053c4fb5a9a",
+    "url": "/todo_hooks/static/js/runtime-main.6d41b28c.js"
+  }
+]);
